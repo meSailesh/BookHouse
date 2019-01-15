ef1d3fbcfcc755ead8a45b9517f11ea348ffe1f1
meSailesh <32452306+meSailesh@users.noreply.github.com>
2019-01-15 11:05:09 +0530

    Add files via upload

---
+++ admin/includes/header.inc.php
@@ -0,0 +1,28 @@
+<?php session_start();
+require('includes/mysql.inc.php');
+$user=$email="";
+?>
+<!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Book House</title>
+   <link rel="icon" href="../images/title-logo.png">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	 <link rel="stylesheet" type="text/css" href="../css/style.css">
+	 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans|Varela+Round|Fjalla+One|Quicksand|Hind+Madurai|Noticia+Text|PT+Sans|Roboto|Ubuntu|Roboto+Condensed|Open+Sans|Prosto+One|Changa|Ropa+Sans" rel="stylesheet">
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
+  <script src="../js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <script src="../js/default.js"></script>
+
+  <script src="main.js"></script>
+  
+</head>
+<body>
+
